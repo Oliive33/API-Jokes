96b71ebf1b666516d886d6a9e7f8cc3cf351b059
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-06-29 15:38:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// https://api.blablagues.net/?rub=blagues
+
+const header = document.getElementById("header");
+const content = document.getElementById("content");
+
+function getJoke() {
+  fetch("https://api.blablagues.net/?rub=blagues")
+    .then((res) => res.json())
+    .then((data) => {
+      const joke = data.data.content;
+      // console.log(joke);
+
+      header.textContent = joke.text_head;
+      content.textContent = joke.text !== "" ? joke.text : joke.text_hidden;
+    });
+}
+getJoke();
+
+document.body.addEventListener("click", getJoke);
+
+//loadar
+
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.className += " hidden";
+  }, 3000);
+});
